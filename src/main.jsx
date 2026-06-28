5a6ca0dc92d206aa6158e94032f9af52cd613aa7
snailhausofficial <snailhausofficial@gmail.com>
2026-06-28 09:49:30 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SnailShopPOS from '../snailshop-nail-pos'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SnailShopPOS />
+  </React.StrictMode>
+)
